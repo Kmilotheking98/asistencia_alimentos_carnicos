894d4d87f6e000865f8b852255489878113d618d
Kmilotheking98 <72001225+Kmilotheking98@users.noreply.github.com>
2022-07-08 03:29:07 -0500

    8/7/2022
    arreglo de la busqueda en la tabla de empleados

--- asistencia_php/buscar.php
+++ asistencia_php/buscar.php
@@ -28,7 +28,7 @@ if(!empty($_POST))
       $query ="SELECT * FROM employees WHERE  `employee_id`  LIKE LOWER('%".$aKeyword[0]."%') OR name LIKE LOWER('%".$aKeyword[0]."%')";
   
 
-     for($i = 1; $i < count($aKeyword); $i++) {
+     for($i = 5; $i < count($aKeyword); $i++) {
         if(!empty($aKeyword[$i])) {
             $query .= " OR  `employee_id`  LIKE '%" . $aKeyword[$i] . "%' OR employee_id LIKE '%" . $aKeyword[$i] . "%'";
         }
@@ -48,14 +48,17 @@ if(!empty($_POST))
         <tr style='background-color:midnightblue; color:#FFFFFF;'>
         <th> # </th> 
         <th> id </th>       
-        <th> NOMBRE </th>
-                
+        <th> Nombre </th>
+        <th> Apellido </th>       
+        <th> DNI </th>                
+        <th> Fecha de Nacimiento </th>       
+        <th> Residencia </th>                
         </tr>
         </thead>
         ";
         While($row = $result->fetch_assoc()) {   
             $row_count++;   
-            echo "<tr><td>".$row_count." </td><td>". resaltar_frase($row['employee_id'] ,$_POST['buscar']) . "</td><td>". resaltar_frase($row['name'] ,$_POST['buscar']) . "</td></tr>";
+            echo "<tr><td>".$row_count." </td><td>". resaltar_frase($row['employee_id'] ,$_POST['buscar']) . "</td><td>". resaltar_frase($row['name'] ,$_POST['buscar']) .resaltar_frase($row['last_name'] ,$_POST['buscar']) . "</td><td>". resaltar_frase($row['dni'] ,$_POST['buscar']).resaltar_frase($row['date_birth'] ,$_POST['buscar']) . "</td><td>". resaltar_frase($row['home'] ,$_POST['buscar']) .  "</td></tr>";
         }
         echo "</table>";
 	
@@ -68,7 +71,7 @@ if(!empty($_POST))
         echo "<br><br><table class='table table-striped'>";
         While($row = $result->fetch_assoc()) {   
             $row_count++;   
-            echo "<tr><td>".$row_count." </td><td>". resaltar_frase($row['employee_id'] ,$_POST['buscar']) . "</td><td>". resaltar_frase($row['name'] ,$_POST['buscar']) . "</td></tr>";
+            echo "<tr><td>".$row_count." </td><td>". resaltar_frase($row['employee_id'] ,$_POST['buscar']) . "</td><td>". resaltar_frase($row['name'] ,$_POST['buscar']) .  resaltar_frase($row['last_name'] ,$_POST['buscar']) . "</td><td>". resaltar_frase($row['dni'] ,$_POST['buscar']) . resaltar_frase($row['date_birth'] ,$_POST['buscar']) . "</td><td>". resaltar_frase($row['home'] ,$_POST['buscar']) .  "</td></tr>";
         }
         echo "</table>";
 	
