b8ff4a192eb6f2a3b6db467866167f6d25e40418
Andres Brown <77754732+1AndresBrown1@users.noreply.github.com>
2022-07-26 10:01:10 -0500

    commit Brown

--- asistencia_php/employees.php
+++ asistencia_php/employees.php
@@ -1,47 +1,63 @@
+
+
 <?php
+include_once "slidernavbar.php";
 include_once "header.php";
-include_once "nav.php";
 include_once "functions.php";
 $employees = getEmployees();
 include("conexion.php");
 ?>
 
+<section class="home">
+    <div class="container cont__me">
+        <div class="employees__content">
+            <a href="employee_add.php" type="button" class="btn btn-warning">
+                <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor"
+                    class="bi bi-person-plus-fill" viewBox="0 0 16 16">
+                    <path d="M1 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H1zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6z" />
+                    <path fill-rule="evenodd"
+                        d="M13.5 5a.5.5 0 0 1 .5.5V7h1.5a.5.5 0 0 1 0 1H14v1.5a.5.5 0 0 1-1 0V8h-1.5a.5.5 0 0 1 0-1H13V5.5a.5.5 0 0 1 .5-.5z" />
+                </svg>
+                &nbspAdd new employee
+            </a>
 
 
-<div class="row">
-    <div class="col-12">
-        <h1 class="text-center">Employees</h1>
-    </div>
-    <div class="col-12">
-        <a href="employee_add.php" class="btn btn-info mb-2">Add new employee <i class="fa fa-plus"></i></a>
+            <div class="buscar__employees__input">
+                <form action="buscar_employee.php" method="post">
+                    <div class="form-group mb-3">
+                        <label class="" style="font-size: 27px;" for="name">Please enttter the name of the
+                            employee</label>
+                        <h3 class="mb-4">Name:</h3>
+                        <input style="width: 500px;" placeholder="search the name" class="form-control" type="text"
+                            name="buscar" id="">
+                    </div>
+                    <div class="form-group">
+                        <input class="btn btn__me" type='submit' value="Search the employee">
 
-    </div>
-    
+                        </input>
+                    </div>
+                </form>
+            </div>
+        </div>
 
-    <div>
-        <form action="buscar_employee.php" method="post">
-            <input type="text" name="buscar" id="">
-            <input type="submit" value="Search">
 
-        </form>
-    </div>
-    <div class="col-12">
-        <div class="table-responsive">
-            <table class="table">
-                <thead>
-                    <tr>
-                        <th>Employee ID</th>
-                        <th>Name</th>
-                        <th>last Name</th>
-                        <th>DNI</th>
-                        <th>Date Birth</th>
-                        <th>Home</th>
-                        <th>Edit</th>
-                        <th>Delete</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <?php foreach ($employees as $employee) { ?>
+        <div class="tabla__employees rounded border">
+            <div class="table-responsive">
+                <table class="table">
+                    <thead>
+                        <tr>
+                            <th>Employee ID</th>
+                            <th>Name</th>
+                            <th>last Name</th>
+                            <th>DNI</th>
+                            <th>Date Birth</th>
+                            <th>Home</th>
+                            <th>Edit</th>
+                            <th>Delete</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php foreach ($employees as $employee) { ?>
                         <tr>
                             <td>
                                 <?php echo $employee->cod ?>
@@ -61,34 +77,26 @@ include("conexion.php");
                             <td>
                                 <?php echo $employee->home ?>
                             </td>
-                            
+
                             <td>
                                 <a class="btn btn-warning" href="employee_edit.php? cod=<?php echo $employee->cod ?>">
-                                Edit <i class="fa fa-edit"></i>
-                            </a>
+                                    Edit <i class="fa fa-edit"></i>
+                                </a>
                             </td>
                             <td>
                                 <a class="btn btn-danger" href="employee_delete.php?cod=<?php echo $employee->cod ?>">
-                                Delete <i class="fa fa-trash"></i>
-                            </a>
+                                    Delete <i class="fa fa-trash"></i>
+                                </a>
                             </td>
                         </tr>
-                    <?php } ?>
-                </tbody>
-            </table>
+                        <?php } ?>
+                    </tbody>
+                </table>
+            </div>
+
         </div>
+
     </div>
-</div>
-
-
-
-
-
-
-
-
-
-
+</section>
 
 <?php
-include_once "footer.php";
--- asistencia_php/header.php
+++ asistencia_php/header.php
@@ -1,16 +1,19 @@
 <!DOCTYPE html>
-<html lang="es">
+<html lang="en">
 <head>
     <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Attendance system with PHP - By Parzibyte</title>
-    <link rel="stylesheet" href="css/bootstrap.min.css">
-    <link rel="stylesheet" href="css/all.min.css">
-    <style>
-        body {
-            padding-top: 80px;
-        }
-    </style>
+    <link rel="stylesheet" href="./bootstrap5/css/bootstrap.css">
+    <link rel="stylesheet" href="./style.css">
+      <!----===== Boxicons CSS ===== -->
+      <link href='https://unpkg.com/boxicons@2.1.1/css/boxicons.min.css' rel='stylesheet'>
+    <title>Document</title>
 </head>
 <body>
-<main class="container-fluid">
+
+
+<script src="script.js"></script>
+<script src="./bootstrap5/js/bootstrap.js"></script>
+</body>
+</html>
