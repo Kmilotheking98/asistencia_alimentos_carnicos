9ad7276f504c922d8256786e8c79d1d0c6b2a674
Andres Brown <77754732+1AndresBrown1@users.noreply.github.com>
2022-08-01 11:45:57 -0500

    Update attendance_register.php

--- asistencia_php/attendance_register.php
+++ asistencia_php/attendance_register.php
@@ -1,86 +1,94 @@
 <?php
+include_once "slidernavbar.php";
 include_once "header.php";
-include_once "nav.php";
+
 ?>
-<div class="row" id="app">
-    <div class="col-12">
-        <h1 class="text-center">Attendance</h1>
-    </div>
-    <div class="col-12">
-        <div class="form-inline mb-2">
-            <label for="date">Date: &nbsp;</label>
-            <input @change="refreshEmployeesList" v-model="date" name="date" id="date" type="date" class="form-control">
-            <button @click="save" class="btn btn-success ml-2">Save</button>
+
+
+<section class="home">
+    <div class="container cont__me employees__content">
+
+
+        <div id="app">
+            <div class="">
+                <div class="form-inline mb-2">
+                    <label for="date">Date: &nbsp;</label>
+                    <input @change="refreshEmployeesList" v-model="date" name="date" id="date" type="date"
+                        class="form-control">
+                    <button @click="save" class="btn btn-success ml-2">Save</button>
+                </div>
+            </div>
+            <div class="col-12">
+                <div class="table-responsive">
+                    <table class="table">
+                        <thead>
+                            <tr>
+                                <th>
+                                    ID Employee
+                                </th>
+                                <th>
+                                    Employee
+                                </th>
+                                <th>
+                                    Job
+                                </th>
+                                <th>
+                                    Status
+                                </th>
+                                <th>
+                                    Status event
+                                </th>
+                                <th>
+                                    Turn
+                                </th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr v-for="employee in employees">
+                                <td>{{employee.cod}}</td>
+                                <td>{{employee.name}}</td>
+                                <td>
+                                    <select v-model="employee.job" class="form-control">
+                                        <option disabled value="unset">--Select--</option>
+                                        <option value="Cajero">Cajero</option>
+                                        <option value="Vendedor">Vendedor</option>
+                                    </select>
+                                </td>
+                                <td>
+                                    <select v-model="employee.status" class="form-control">
+                                        <option disabled value="unset">--Select--</option>
+                                        <option value="presence">Presence</option>
+                                        <option value="absence">Absence</option>
+                                    </select>
+                                </td>
+                                <td>
+                                    <select v-model="employee.status_event" class="form-control">
+                                        <option disabled value="unset">--Select--</option>
+                                        <option value="si">Si</option>
+                                        <option value="no">No</option>
+                                    </select>
+                                </td>
+                                <td>
+                                    <select v-model="employee.turn" class="form-control">
+                                        <option disabled value="unset">--Select--</option>
+                                        <option value="1">1</option>
+                                        <option value="2">2</option>
+                                        <option value="3">3</option>
+                                        <option value="4">4</option>
+                                        <option value="5">5</option>
+                                        <option value="6">6</option>
+                                        <option value="7">7</option>
+                                    </select>
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
         </div>
-    </div>
-    <div class="col-12">
-        <div class="table-responsive">
-            <table class="table">
-                <thead>
-                    <tr>
-                    <th>
-                            ID Employee
-                        </th>
-                        <th>
-                            Employee
-                        </th>
-                        <th>
-                            Job
-                        </th>
-                        <th>
-                            Status
-                        </th>
-                        <th>
-                            Status event
-                        </th>
-                        <th>
-                            Turn
-                        </th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr v-for="employee in employees">
-                    <td>{{employee.cod}}</td>
-                        <td>{{employee.name}}</td>
-                        <td>
-                            <select v-model="employee.job" class="form-control">
-                                <option disabled value="unset">--Select--</option>
-                                <option value="Cajero">Cajero</option>
-                                <option value="Vendedor">Vendedor</option>
-                            </select>
-                        </td>
-                        <td>
-                            <select v-model="employee.status" class="form-control">
-                                <option disabled value="unset">--Select--</option>
-                                <option value="presence">Presence</option>
-                                <option value="absence">Absence</option>
-                            </select>
-                        </td>
-                        <td>
-                            <select v-model="employee.status_event" class="form-control">
-                                <option disabled value="unset">--Select--</option>
-                                <option value="si">Si</option>
-                                <option value="no">No</option>
-                            </select>
-                        </td>
-                        <td>
-                            <select v-model="employee.turn" class="form-control">
-                                <option disabled value="unset">--Select--</option>
-                                <option value="1">1</option>
-                                <option value="2">2</option>
-                                <option value="3">3</option>
-                                <option value="4">4</option>
-                                <option value="5">5</option>
-                                <option value="6">6</option>
-                                <option value="7">7</option>
-                            </select>
-                        </td>
-                    </tr>
-                </tbody>
-            </table>
-        </div>
-    </div>
-</div>
+</section>
+
+
 <script src="js/vue.min.js"></script>
 <script src="js/vue-toasted.min.js"></script>
 <script>
@@ -158,7 +166,7 @@ include_once "nav.php";
                         employees[index].status = attendanceDetail.status;
                         employees[index].status_event = attendanceDetail.status_event;
                         employees[index].turn = attendanceDetail.turn;
-                        
+
                     }
                 });
                 // Let Vue do its magic ;)
@@ -168,4 +176,3 @@ include_once "nav.php";
     });
 </script>
 <?php
-include_once "footer.php";
