7f869e2dd698a0ce6c064e17cd95fb283cde7b42
Kmilotheking98 <72001225+Kmilotheking98@users.noreply.github.com>
2022-08-13 01:25:49 -0500

    tema dark
    tema oscuro solucionado

--- asistencia_php/script.js
+++ asistencia_php/script.js
@@ -1,11 +1,11 @@
-const body = document.querySelector('body'),
-      sidebar = body.querySelector('nav'),
-      toggle = body.querySelector(".toggle"),
-      searchBtn = body.querySelector(".search-box"),
-      modeSwitch = body.querySelector(".toggle-switch"),
-      modeText = body.querySelector(".mode-Dark");
+const body = document.querySelector('body');
+const sidebar = body.querySelector('nav');
+const toggle = body.querySelector(".toggle");
+const searchBtn = body.querySelector(".search-box");
+const modeSwitch = body.querySelector(".toggle-switch");
+const modeText = body.querySelector(".mode-Dark");
 
-
+load();
 toggle.addEventListener("click" , () =>{
     sidebar.classList.toggle("close");
 })
@@ -14,8 +14,9 @@ searchBtn.addEventListener("click" , () =>{
     sidebar.classList.remove("close");
 })
 
-modeSwitch.addEventListener("click" , () =>{
+modeSwitch.addEventListener("click" , e =>{
     body.classList.toggle("dark");
+    store(body.classList.contains("dark"))
 
     if(body.classList.contains("dark")){
         modeText.innerText = "Dark mode";
@@ -24,3 +25,18 @@ modeSwitch.addEventListener("click" , () =>{
 
     }
 });
+
+console.log(body)
+
+function load() {
+    const darkmode = localStorage.getItem("dark")
+
+    if(!darkmode){
+        store('false')
+    }else if(darkmode=='true'){
+        body.classList.add('dark')
+    }
+}
+function store(value) {
+    localStorage.setItem('dark', value)
+}
