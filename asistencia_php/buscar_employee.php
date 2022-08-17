6bf9c1161ed3bb406605d8c36d861f34f646a88c
Kmilotheking98 <72001225+Kmilotheking98@users.noreply.github.com>
2022-08-17 00:27:16 -0500

    RESPONSIVE 2.0
    -se mejoro el responsive a todos los componentes

--- asistencia_php/buscar_employee.php
+++ asistencia_php/buscar_employee.php
@@ -11,38 +11,73 @@ if ($_SESSION['permit'] ==2) {
 
 ?>
 
+<!DOCTYPE html>
 
-<section class="home">
-    <div class="container cont__me employees__content">
+<body>
 
-            <?php
+        <?php
           $aKeyword= explode(" ", $_POST['buscar']);
         ?>
 
-            <div class='d-flex justify-content-end'>
+    <div class="employees-home ">
+        <div class="col-12">
+              <h1 class="text-left">BUSQUEDA DE EMPLEADOS</h1>
+        </div>
+        <div class="barra-top">
+            <div class="container-barra  bd-highlight">
+                <div class="barra-buscar-container p-2 bd-highlight">
 
-            <a href="employee_add.php" type="button" class="btn btn-warning me-3">
-                <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor"
-                    class="bi bi-person-plus-fill" viewBox="0 0 16 16">
-                    <path d="M1 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H1zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6z" />
-                    <path fill-rule="evenodd"
-                        d="M13.5 5a.5.5 0 0 1 .5.5V7h1.5a.5.5 0 0 1 0 1H14v1.5a.5.5 0 0 1-1 0V8h-1.5a.5.5 0 0 1 0-1H13V5.5a.5.5 0 0 1 .5-.5z" />
-                </svg>
-                &nbspRegistrar Empleado
-            </a>
-    
-                <form  class='d-flex' action="buscar_employee.php" method="post">
-                    <input class="form-control me-3" placeholder="¿Quieres seguir buscando?" style="width: 340px;" type="text" name="buscar" id="">
-                    <input class="btn btn__me" type="submit" value="Buscar">
-                </form>
+                    <form class='d-flex' action="buscar_employee.php" method="post">
+                        <input placeholder="¿Qué deceas buscar?" class="form-control me-3" type="text" name="buscar"
+                            id="">
+                        <input class="btn btn__me" type='submit' value="Buscar"></input>
+                    </form>
+
+                </div>
+                <div class="p-2 bd-highlight">
+
+                    <div class="d-grid gap-2 d-md-flex justify-content-md-end">
+                    
+                        <a href="employee_add.php" type="button" class="btn btn-warning me-md-3">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor"
+                                class="bi bi-person-plus-fill" viewBox="0 0 16 16">
+                                <path d="M1 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H1zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6z" />
+                                <path fill-rule="evenodd"
+                                    d="M13.5 5a.5.5 0 0 1 .5.5V7h1.5a.5.5 0 0 1 0 1H14v1.5a.5.5 0 0 1-1 0V8h-1.5a.5.5 0 0 1 0-1H13V5.5a.5.5 0 0 1 .5-.5z" />
+                            </svg>                            
+                            &nbspRegistrar Empleado
+                        </a>
+                            <?php
+
+                                if (isset($_SESSION['msj'])){ 
+                                    $respuesta = $_SESSION['msj'];?> 
+                                        <script>
+                                            Swal.fire(
+                                            '<?php echo $_SESSION['info']; ?>',
+                                            '<?php echo $respuesta; ?>',
+                                            //'REGISTRO EXITOSO',
+                                            'success'
+                                                    )
+                                        </script>    
+                                            
+                                 <?php 
+                                unset($_SESSION['info']);
+                                unset($_SESSION['msj']);
+                            } ?> 
+                    </div>
+
+                </div>
             </div>
+        </div>
 
+        <div class='table-container'>
 
-            <div class="tabla__employees rounded border">
-                <div class="table-responsive">
-                    <table class="table table-striped">
-                        <thead>
-                            <tr>
+        <div class="tabla__employees rounded ">
+            <div class="table-responsive">
+                <table class="table">
+                <?php  if($_POST['buscar'] !=""){ ?>
+                    <thead>
+                        <tr>
                             <th>CODIGO</th>
                             <th>NOMBRE</th>
                             <th>APELLIDOS</th>
@@ -52,11 +87,11 @@ if ($_SESSION['permit'] ==2) {
                             <th>RESIDENCIA</th>
                             <th>EDITAR</th>
                             <th>ELIMINAR</th>
-                            </tr>
-                        </thead>
+                        </tr>
+                    </thead>
 
                         <?php 
-                    
+                    }
                     // largo de la busqueda 
                     $query ="SELECT * FROM employees WHERE  `cod`  LIKE LOWER('%".$aKeyword[0]."%') OR name LIKE LOWER('%".$aKeyword[0]."%') OR last_name LIKE LOWER('%".$aKeyword[0]."%') OR dni LIKE LOWER('%".$aKeyword[0]."%') OR type_contract LIKE LOWER('%".$aKeyword[0]."%') OR date_birth LIKE LOWER('%".$aKeyword[0]."%') OR home LIKE LOWER('%".$aKeyword[0]."%')";
                     // cantidad de capos que recorrer 
@@ -70,7 +105,7 @@ if ($_SESSION['permit'] ==2) {
                          $numero = mysqli_num_rows($result);
 
                          if( mysqli_num_rows($result) > 0 AND $_POST['buscar'] != '') {
-                            
+                            echo"<br> <p style='font-size:30px;font-weight:bold;'>Resultados de la busqueda...</p><br> ";                            
                         while($employee=mysqli_fetch_row($result)){ ?>
 
                         <tbody>
@@ -97,34 +132,59 @@ if ($_SESSION['permit'] ==2) {
                                     <?php echo $employee['6'] ?>
                                 </td>                                
                                 <td>
-                                    <a class="btn btn-warning"
-                                        href="employee_edit.php?cod=<?php echo $employee['0'] ?>">
-                                        Editar <i class="fa fa-edit"></i>
-                                    </a>
+                                <a class="btn btn-warning" href="employee_edit.php? cod=<?php echo $employee->cod ?>">
+                                    Editar
+                                     <!-- <i class="bx bx-edit-alt icon"> -->
+                                     <i></i>
+                                </a>
                                 </td>
                                 <td>
-                                    <a class="btn btn-danger"
-                                        href="employee_delete.php?cod=<?php echo $employee['1']  ?>">
-                                        Eliminar <i class="fa fa-trash"></i>
-                                    </a>
+
+                                <a class="btn btn-danger" href="#" onclick="question(<?php echo $employee->cod ?>)">
+                                    Borrar 
+                                    <!-- <i class="bx bx-trash-alt icon" > -->
+                                    <i></i>
+                                </a>
                                 </td>
                             </tr>
-                            <?php   }}else{
-                        echo"<br> <p style='font-size:30px;font-weight:bold;'>No se encontro el empleado</p><br> ";
-                    } ?>
-                        </tbody>
-                    </table>
-                </div>
+                            <?php   }}elseif($_POST['buscar']==""){
+                                echo"<br> <p style='font-size:30px;font-weight:bold;'>Por favor ingresar dato a buscar del empleado...</p><br> ";
+                            }else{
+                                echo"<br> <p style='font-size:30px;font-weight:bold;'>No se encontro el empleado...</p><br> ";
+                            } ?>
+                    </tbody>
+                    <script type="text/javascript">
+                  function question(cod){
+
+                        Swal.fire({
+                            title: '¿Está seguro?',
+                            text: "¡No podrás revertir esto!",
+                            icon: 'warning',
+                            showCancelButton: true,
+                            confirmButtonColor: '#695CFE',
+                            cancelButtonColor: '#d33',
+                            confirmButtonText: '¡Si, Borralo!',
+                            cancelButtonText: '¡No, Cancelar!'
+                            }).then((result) => {
+                            if (result.isConfirmed) {
+                                window.location.href = "employee_delete.php?cod="+cod;
+                               
+                            }
+                            })
+                                
+                  }
+
+                    </script>                          
+                </table>
             </div>
 
+        </div>
 
-       
-</section>
+        </div>
 
+    </div>
+</body>
 
-
-
-
-
+</html>
 
 <?php
