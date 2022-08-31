11f7b3cbe60b31a598edfadef201b35916396b62
Kmilotheking98 <72001225+Kmilotheking98@users.noreply.github.com>
2022-08-31 05:13:42 -0500

    login 2.0
    -se mejoro la interfaz del login
    - se mejoro el responsive en los componentes a varias resoluciones

--- asistencia_php/partials/header.php
+++ asistencia_php/partials/header.php
@@ -1,4 +1,9 @@
 <header>
 <link rel="icon" type="image/png" href="img/logo.png">
-  <a href="/php-login">ALIMENTOS CARNICOS</a>
+  <div class="imagelog">
+                <span class="imagelog">
+                    <img href="/asistencia_alimentos_carnicos/asistencia_php/login.php" src="img/logologin.png" alt="Alimentos Carnicos">
+                    <a href="/asistencia_alimentos_carnicos/asistencia_php/login.php" >ALIMENTOS CARNICOS</a>
+                </span>
+    </div>
 </header>
