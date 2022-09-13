1c6c8091b95acdec7f907fd34958e810c65f1f46
Kmilotheking98 <72001225+Kmilotheking98@users.noreply.github.com>
2022-09-13 04:50:02 -0500

    bug del slidernav

--- asistencia_php/slidernavbar.php
+++ asistencia_php/slidernavbar.php
@@ -138,6 +138,7 @@ if($permit == 1){
                 <?php }?>
 
             <div class="bottom-content">
+                <br></br>
                 <li class="mode">
                     <div class="sun-moon">
                         <i class='bx bx-moon icon moon'></i>
@@ -155,7 +156,7 @@ if($permit == 1){
                         <span class="text nav-text">Logout</span>
                     </a>
                 </li>
-
+                <br></br>
                 
 
                
