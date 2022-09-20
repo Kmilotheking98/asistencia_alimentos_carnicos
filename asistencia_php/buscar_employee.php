fa2e4dc83dc4a29d3fba17647a596d36a3a19994
Kmilotheking98 <72001225+Kmilotheking98@users.noreply.github.com>
2022-09-20 00:22:20 -0500

    ...

--- asistencia_php/buscar_employee.php
+++ asistencia_php/buscar_employee.php
@@ -116,27 +116,31 @@ if ($_SESSION['permit'] == 2) {
                                     <?php echo $employee['6'] ?>
                                 </td>  
                                 <td>
-                                    <a class="btn btn-warning"
-                                        href="employee_edit.php?cod=<?php echo $employee['0'] ?>">
-                                        Editar
-                                     <!-- <i class="bx bx-edit-alt icon"> -->
-                                     <i></i>
-                                    </a>
-                                </td>
-                                <td> 
-                                    <?php
-                                    // capturar codigo en variable para que pueda eliminar por mediante la busqueda  
-                                    $cod = $employee['0']; 
-                                   // echo"$cod";
-                                     ?>                                  
-                                <a class="btn btn-danger" href="#" onclick="question(<?php echo $cod ?>)">                                    
-                                    <!-- <a 
-                                        href="employee_delete.php?cod="> -->
-                                        Borrar 
-                                        <!-- <i class="bx bx-trash-alt icon" > -->
-                                        <i></i>
-                                    </a>
-                                </td>                                
+                            <a class="btn btn-warning" href="employee_edit.php? cod=<?php echo $employee->cod ?>">
+                                  
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil-square" viewBox="0 0 16 16">
+  <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/>
+  <path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z"/>
+</svg>
+
+
+                                </a>
+                            </td>
+                            <td>
+
+                                <a class="btn btn-danger" href="#" onclick="question(<?php echo $employee->cod ?>)">
+                                    
+
+                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash-fill" viewBox="0 0 16 16">
+  <path d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0z"/>
+</svg>
+                        
+                                  
+                                </a>
+                            </td>
+
+                                
+                                           
                                                          
                             </tr>
                             <?php   }}elseif($_POST['buscar']==""){
@@ -148,22 +152,21 @@ if ($_SESSION['permit'] == 2) {
                     <script type="text/javascript">
                   function question($cod){
 
-                        Swal.fire({
+                    Swal.fire({
                             title: '¿Está seguro?',
                             text: "¡No podrás revertir esto!",
                             icon: 'warning',
                             showCancelButton: true,
-                            confirmButtonColor: '#695CFE',
-                            cancelButtonColor: '#d33',
+                            confirmButtonColor: '#d33',
+                            cancelButtonColor: '#695CFE',
                             confirmButtonText: '¡Si, Borralo!',
                             cancelButtonText: '¡No, Cancelar!'
                             }).then((result) => {
                             if (result.isConfirmed) {
-                                window.location.href = "employee_delete.php?cod="+$cod;
+                                window.location.href = "employee_delete.php?cod="+cod;
                                
                             }
-                            })
-                                
+                            })                                
                   }
 
                     </script>                          
@@ -178,5 +181,3 @@ if ($_SESSION['permit'] == 2) {
 </body>
 
 </html>
-
-<?php
